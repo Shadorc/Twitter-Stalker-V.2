88aec5246100aa3db7e4df0920ca272f51282bc5
Shadorc <bi-crosseur@hotmail.fr>
2015-07-09 20:59:58 +0200

    Change logs

--- src/me/shadorc/twitterstalker/statistics/Stats.java
+++ src/me/shadorc/twitterstalker/statistics/Stats.java
@@ -89,7 +89,7 @@ public class Stats {
 		for(int i = 1; user.getTweetsAnalyzed() < tweetsToAnalyze; i++) {
 
 			RateLimitStatus rls = Frame.getTwitter().getRateLimitStatus().get("/statuses/user_timeline");
-			System.out.println("User timeline : " + rls.getRemaining() + " / " + rls.getLimit() + ", reset in " + (rls.getSecondsUntilReset()/60) + "min " + (int) ((rls.getSecondsUntilReset()/60f - (int) (rls.getSecondsUntilReset()/60f))*60) + "s");
+			System.out.println("User timeline : " + rls.getRemaining() + "/" + rls.getLimit() + ", reset in " + (rls.getSecondsUntilReset()/60) + "min " + (int) ((rls.getSecondsUntilReset()/60f - (int) (rls.getSecondsUntilReset()/60f))*60) + "s");
 
 			List <Status> timeline = (statusList == null) ? Frame.getTwitter().getUserTimeline(user.getName(), new Paging(i, 200)) : statusList;
 
@@ -126,7 +126,7 @@ public class Stats {
 				int secure = user.getMentionsAnalyzed();
 
 				RateLimitStatus rls = Frame.getTwitter().getRateLimitStatus().get("/statuses/mentions_timeline");
-				System.out.println("Mentions timeline : " + rls.getRemaining() + " / " + rls.getLimit() + ", reset in " + (rls.getSecondsUntilReset()/60) + "min " + (int) ((rls.getSecondsUntilReset()/60f - (int) (rls.getSecondsUntilReset()/60f))*60) + "s");
+				System.out.println("Mentions timeline : " + rls.getRemaining() + "/" + rls.getLimit() + ", reset in " + (rls.getSecondsUntilReset()/60) + "min " + (int) ((rls.getSecondsUntilReset()/60f - (int) (rls.getSecondsUntilReset()/60f))*60) + "s");
 
 				try {
 					for(Status status : Frame.getTwitter().getMentionsTimeline(new Paging(i, 200))) {
